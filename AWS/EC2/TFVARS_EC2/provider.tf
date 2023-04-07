a6eb5fbf077dfd332a12891e5fa18af962a7f955
Samra <samra.kamal.160494@gmail.com>
2023-04-07 21:39:10 +0530

    main:modify commit

--- AWS/EC2/TFVARS_EC2/provider.tf
+++ AWS/EC2/TFVARS_EC2/provider.tf
@@ -1,6 +1,6 @@
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIARRTG5Y3P6FHIB2R2"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
